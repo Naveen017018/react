46fc851e1651bda32090c92d5ff676597671984f
Naveen017018 <113774658+Naveen017018@users.noreply.github.com>
2023-04-21 08:25:39 +0530

    Add files via upload

---
+++ old react/REACTAPI/src/App.js
@@ -0,0 +1,21 @@
+import TodoFunc from './Components/TodoFunc';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header>     
+      <h1>Todo List</h1> 
+      </header>
+    
+    </div>
+    <div>
+       <TodoFunc />
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
